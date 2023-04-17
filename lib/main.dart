28cbaf9546e0f940b28c4ac626414a6e0c1acbf4
Mayeavx <mayoodukoya@gmail.com>
2023-04-17 12:08:54 +0100

    initial build

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:twitter_clone/core/common/error_text.dart';
+import 'package:twitter_clone/core/common/loader.dart';
+import 'package:twitter_clone/models/user.dart' as model;
 import 'package:twitter_clone/features/auth/screens/details_screen.dart';
 import 'package:twitter_clone/features/auth/screens/login_screen.dart';
 import 'package:twitter_clone/features/auth/screens/signup_screen.dart';
+import 'package:twitter_clone/router.dart';
 import 'package:twitter_clone/theme/palette.dart';
+import 'features/auth/controller/auth_controller.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -15,21 +22,67 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
+  @override
+  ConsumerState createState() => _MyAppState();
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends ConsumerState<MyApp> {
+  model.User? userModel;
+  int ctr = 0;
 
-  // This widget is the root of your application.
+  void getData(WidgetRef ref, User user) async {
+    userModel = await ref
+        .read(authControllerProvider.notifier)
+        .getUserData(user.uid)
+        .first;
+    ref.watch(userProvider.notifier).update((state) => userModel);
+    if (ctr == 0) {
+      ctr++;
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Twitter Clone',
-      theme: Palette.lightsOutModeAppTheme,
-      home: const DetailsScreen(name: 'mayo', email: 'maayo@email.com', dob: '14 aug 1998'),
-    );
+    return ref.watch(authStateChangeProvider).when(
+          data: (data) => MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            theme: Palette.lightsOutModeAppTheme,
+            routerDelegate: RoutemasterDelegate(
+              routesBuilder: (context) {
+                if (data != null) {
+                  getData(ref, data);
+                  if(userModel == null){
+                    return loggedOutRoute;
+                  }
+                }
+                return loggedOutRoute;
+              },
+            ),
+            routeInformationParser: const RoutemasterParser(),
+          ),
+          error: (err, stack) => ErrorText(error: err.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
 
-
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Twitter Clone',
+//       theme: Palette.lightsOutModeAppTheme,
+//       home: const DetailsScreen(name: 'mayo', email: 'maayo@email.com', dob: '14 aug 1998'),
+//     );
+//   }
+// }
