e087f068caca918c3d7a99061943b1beb3ffd108
Mayeavx <mayoodukoya@gmail.com>
2023-05-23 12:15:33 +0100

    initial build

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_clone/core/common/widgets/tweet_card.dart';
-import 'package:twitter_clone/features/auth/screens/login_screen.dart';
 import 'package:twitter_clone/features/feed/screens/feed_screen.dart';
 import 'package:twitter_clone/features/notifications/screens/notifications_screen.dart';
-import 'package:twitter_clone/features/profiles/screens/profile_screen.dart';
 import 'package:twitter_clone/features/search/screen/search_screen.dart';
-import 'package:twitter_clone/features/tweets/screens/create_tweet_screen.dart';
-import 'package:twitter_clone/models/tweet.dart';
 
 class Constants{
   static String defaultAvatar = 'https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png';
